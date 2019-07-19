0f34a847221a07bd3e6fe49312ad54685e643de8
Deon Lee <deonlee95@Deons-MacBook-Pro.local>
2019-07-19 10:23:43 +0800

    Created scene view

--- BuildAR/SceneViewController.swift
+++ BuildAR/SceneViewController.swift
@@ -5,15 +5,40 @@
 //  Created by Deon Lee on 16/7/19.
 //  Copyright © 2019 Tay Jin Wen. All rights reserved.
 //
+
 import UIKit
 import SceneKit
-import ARKit
-import Firebase
 
-class SceneViewController: UIViewController{
+class SceneViewController: UIViewController {
+    
+    var sceneView:SCNView!
+    var scene:SCNScene!
+    
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
+        setupScene()
     }
+    
+    func setupScene(){
+        sceneView = (self.view as! SCNView)
+        sceneView.allowsCameraControl = true
+        sceneView.autoenablesDefaultLighting = true
+        scene = SCNScene(named: "art.scnassets/SceneKit/mainScene.scn")
+        sceneView.scene = scene
+    }
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
 }
